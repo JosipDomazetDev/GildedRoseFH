b16d50fd54676be24a57865a3462a92c47dc7dd0
Josip Domazet <josip.domazet.corp@gmail.com>
2024-09-22 12:45:52 +0200

    refactor: flattened if structure with guard clauses, consts, intellj recommendations, tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,61 +1,67 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        for (Item item : items) {
+            // Guard Clause: Sulfuras does not change in quality or sellIn
+            if (item.name.equals(SULFURAS)) {
+                continue;
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            // Decrement sellIn for all items except Sulfuras
+            item.sellIn--;
+
+            // Handle Aged Brie
+            if (item.name.equals(AGED_BRIE)) {
+                increaseQuality(item);
+                if (item.sellIn < 0) {
+                    increaseQuality(item);
+                }
+
+                continue;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            // Handle Backstage Passes
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                    continue;
                 }
+                increaseQuality(item);
+                if (item.sellIn < 10) {
+                    increaseQuality(item);
+                }
+                if (item.sellIn < 5) {
+                    increaseQuality(item);
+                }
+                continue;
+            }
+
+            // Handle Normal Items
+            decreaseQuality(item);
+            if (item.sellIn < 0) {
+                decreaseQuality(item);
             }
         }
     }
