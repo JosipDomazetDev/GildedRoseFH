4193c776bb86c8a160c17f45529dd67ffa31b294
Josip Domazet <josip.domazet.corp@gmail.com>
2024-09-22 12:54:13 +0200

    removed all else cases

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -63,14 +63,15 @@ class GildedRose {
         // Handle Backstage Passes
         if (item.sellIn < 0) {
             item.quality = 0;
-        } else {
+            return;
+        }
+
+        increaseQuality(item);
+        if (item.sellIn < 10) {
             increaseQuality(item);
-            if (item.sellIn < 10) {
-                increaseQuality(item);
-            }
-            if (item.sellIn < 5) {
-                increaseQuality(item);
-            }
+        }
+        if (item.sellIn < 5) {
+            increaseQuality(item);
         }
     }
 
